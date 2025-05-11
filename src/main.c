2d35156b009570bbeb002eb631ad2456d08473b5
having11 <gatoninja236@gmail.com>
2025-05-10 22:36:21 -0700

    fixes

--- src/main.c
+++ src/main.c
@@ -7,28 +7,30 @@
  * Input data in the buffer:
  * 0 - uint32_t -> number of samples
  * 4 - float * -> start of sample 0 to sample N
- * 
+ *
  * Output data in the buffer:
  * 0 - float -> weighted mean of samples
  */
 
 typedef enum
 {
-	kCalculateNoCommand		= 0, /* Go to idle */
-	kCalculateWindow     = 1, /* Calculate window */
+  kCalculateNoCommand = 0, /* Go to idle */
+  kCalculateWindow = 1,    /* Calculate window */
 } SignaloidSoCCommand;
 
-static float getWeightedMean(float * values, size_t count)
+static float getWeightedMean(float *values, size_t count)
 {
   float dist = UxHwFloatDistFromSamples(values, count);
   float variance = UxHwFloatNthMoment(dist, 2);
   float mean = UxHwFloatNthMoment(dist, 1);
 
-  WeightedFloatSample *weightedSamples = (WeightedFloatSample *)malloc(count, sizeof(WeightedFloatSample), __FILE__, __LINE__);
+  WeightedFloatSample *weightedSamples = (WeightedFloatSample *)malloc(count * sizeof(WeightedFloatSample));
 
-  for (size_t i = 0; i < count; i++) {
+  for (size_t i = 0; i < count; i++)
+  {
     float weight = 1.0f;
-    if (variance > 0.0f) {
+    if (variance > 0.0f)
+    {
       float diff = values[i] - mean;
       weight = expf(-(diff * diff) / (2.0f * variance));
     }
@@ -43,93 +45,98 @@ static float getWeightedMean(float * values, size_t count)
   return weightedMean;
 }
 
-int
-main(void)
+int main(void)
 {
-	volatile SignaloidSoCStatus *	mmioStatus		= (SignaloidSoCStatus *) kSignaloidSoCDeviceConstantsStatusAddress;
-	volatile uint32_t *		mmioSoCControl		= (uint32_t *) kSignaloidSoCDeviceConstantsSoCControlAddress;
-	volatile SignaloidSoCCommand *	mmioCommand		= (SignaloidSoCCommand *) kSignaloidSoCDeviceConstantsCommandAddress;
+  volatile SignaloidSoCStatus *mmioStatus = (SignaloidSoCStatus *)kSignaloidSoCDeviceConstantsStatusAddress;
+  volatile uint32_t *mmioSoCControl = (uint32_t *)kSignaloidSoCDeviceConstantsSoCControlAddress;
+  volatile SignaloidSoCCommand *mmioCommand = (SignaloidSoCCommand *)kSignaloidSoCDeviceConstantsCommandAddress;
 
-	volatile double *		MOSIBuffer		= (double *) kSignaloidSoCDeviceConstantsMOSIBufferAddress;
-	volatile uint32_t *		MOSIBufferUInt		= (uint32_t *) kSignaloidSoCDeviceConstantsMOSIBufferAddress;
+  volatile double *MOSIBuffer = (double *)kSignaloidSoCDeviceConstantsMOSIBufferAddress;
+  volatile uint32_t *MOSIBufferUInt = (uint32_t *)kSignaloidSoCDeviceConstantsMOSIBufferAddress;
 
-	volatile double *		MISOBuffer		= (double *) kSignaloidSoCDeviceConstantsMISOBufferAddress;
-	volatile uint32_t *		resultBufferSize	= (uint32_t *) kSignaloidSoCDeviceConstantsMISOBufferAddress;
-	volatile uint8_t *		resultBuffer		= (uint8_t *) (kSignaloidSoCDeviceConstantsMISOBufferAddress + sizeof(uint32_t));
+  volatile double *MISOBuffer = (double *)kSignaloidSoCDeviceConstantsMISOBufferAddress;
+  volatile uint32_t *resultBufferSize = (uint32_t *)kSignaloidSoCDeviceConstantsMISOBufferAddress;
+  volatile uint8_t *resultBuffer = (uint8_t *)(kSignaloidSoCDeviceConstantsMISOBufferAddress + sizeof(uint32_t));
 
-	while (1)
-	{
-		/*
-		 *	Set status to "waitingForCommand"
-		 */
-		*mmioStatus = kSignaloidSoCStatusWaitingForCommand;
+  while (1)
+  {
+    /*
+     *	Set status to "waitingForCommand"
+     */
+    *mmioStatus = kSignaloidSoCStatusWaitingForCommand;
 
-		/*
-		 *	Block until command is issued
-		 */
-		while (*mmioCommand == kCalculateNoCommand) {}
+    /*
+     *	Block until command is issued
+     */
+    while (*mmioCommand == kCalculateNoCommand)
+    {
+    }
 
-		/*
-		 *	Set status to inform host that calculation will start
-		 */
-		*mmioStatus = kSignaloidSoCStatusCalculating;
+    /*
+     *	Set status to inform host that calculation will start
+     */
+    *mmioStatus = kSignaloidSoCStatusCalculating;
 
-		/*
-		 *	Turn on status LED
-		 */
-		*mmioSoCControl = 0xffffffff;
+    /*
+     *	Turn on status LED
+     */
+    *mmioSoCControl = 0xffffffff;
     uint32_t resultSize = sizeof(float);
+    float result;
 
-		switch (*mmioCommand)
-		{	
-			/*
-			 *	All of the following commands parse the inputs in the same way
-			 */
-			case kCalculateWindow:
+    switch (*mmioCommand)
+    {
+    /*
+     *	All of the following commands parse the inputs in the same way
+     */
+    case kCalculateWindow:
+    {
+      // First argument is the number of samples in the distribution
+      uint16_t numSamples = (uint16_t)MOSIBufferUInt[0];
 
-        // First argument is the number of samples in the distribution
-        uint16_t numSamples = (uint16_t)MOSIBufferUInt[0];
+      /*
+       *	Calculate
+       */
+      switch (*mmioCommand)
+      {
+      case kCalculateWindow:
+        /*
+         *	Calculate window's weighted mean natively
+         */
+        result = getWeightedMean((float *)((uint8_t *)MOSIBuffer + sizeof(uint32_t)), numSamples);
+        // Copy the result to the MISO buffer
+        MOSIBuffer[0] = result;
+        break;
+      default:
+        break;
+      }
 
-				/*
-				 *	Calculate
-				 */
-				switch (*mmioCommand)
-				{
-          case kCalculateWindow:
-            /*
-             *	Calculate window's weighted mean natively
-             */
-            float result = getWeightedMean((float *)((uint8_t *)MOSIBuffer + sizeof(uint32_t)), numSamples);
-            // Copy the result to the MISO buffer
-            MOSIBuffer[0] = result;
-            break;
-					default:
-						break;
-				}
+      // TODO: Does this function even exist?
+      // resultSize = UxHwFloatDistributionToByteArray(result, resultBuffer, kSignaloidSoCCommonConstantsMISOBufferSizeBytes - sizeof(uint32_t));
+      *resultBufferSize = resultSize;
 
-				// TODO: Does this function even exist?
-				// resultSize = UxHwFloatDistributionToByteArray(result, resultBuffer, kSignaloidSoCCommonConstantsMISOBufferSizeBytes - sizeof(uint32_t));
-				*resultBufferSize = resultSize;
+      /*
+       *	Set status
+       */
+      *mmioStatus = kSignaloidSoCStatusDone;
+    }
+    break;
 
-				/*
-				 *	Set status
-				 */
-				*mmioStatus = kSignaloidSoCStatusDone;
-				break;
+    default:
+      *mmioStatus = kSignaloidSoCStatusInvalidCommand;
+      break;
+    }
 
-			default:
-				*mmioStatus = kSignaloidSoCStatusInvalidCommand;
-				break;
-		}
+    /*
+     *	Turn off status LED
+     */
+    *mmioSoCControl = 0x00000000;
 
-		/*
-		 *	Turn off status LED
-		 */
-		*mmioSoCControl = 0x00000000;
-
-		/*
-		 *	Block until command is cleared
-		 */
-		while (*mmioCommand != kCalculateNoCommand) {}
-	}
+    /*
+     *	Block until command is cleared
+     */
+    while (*mmioCommand != kCalculateNoCommand)
+    {
+    }
+  }
 }
